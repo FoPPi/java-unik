6c1deb3e3a056ae130ca1684ef555e025d587344
FoPPi <mishk908@gmail.com>
2024-10-11 15:06:06 +0300

    init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,53 @@
 package org.example;
 
-import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = "https://informer.com.ua/dut/java/pr2.csv";
-        List<Transaction> transactions = TransactionCSVReader.readTransactions(filePath);
-
-        TransactionReportGenerator.printAllData(transactions);
-
-        double totalBalance = TransactionAnalyzer.calculateTotalBalance(transactions);
-
-        TransactionReportGenerator.printBalanceReport(totalBalance);
-
-        String monthYear = "01-2024";
-        int transactionsCount = TransactionAnalyzer.countTransactionsByMonth(monthYear, transactions);
-        TransactionReportGenerator.printTransactionsCountByMonth(monthYear, transactionsCount);
-
-        List<Transaction> topExpenses = TransactionAnalyzer.findTopExpenses(transactions);
-        TransactionReportGenerator.printTopExpensesReport(topExpenses);
+        double a = 0, b = 0;
+        Scanner reader = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter two numbers: ");
+            System.out.println("Enter a number: ");
+            try {
+                a = Double.parseDouble(reader.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number");
+                continue;
+            } finally {
+                System.out.println("Done");
+            }
+            System.out.println("Enter b number: ");
+            try {
+                b = Double.parseDouble(reader.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number");
+                continue;
+            } finally {
+                System.out.println("Done");
+            }
+            System.out.println("Enter an operator (+, -, *, /): ");
+            String operator = reader.nextLine();
+            switch (operator) {
+                case "+":
+                    System.out.println(Calculator.addition(a, b));
+                    break;
+                case "-":
+                    System.out.println(Calculator.subtract(a, b));
+                    break;
+                case "*":
+                    System.out.println(Calculator.multiply(a, b));
+                    break;
+                case "/":
+                    System.out.println(Calculator.divide(a, b));
+                    break;
+                default:
+                    System.out.println("Invalid operator");
+            }
+            System.out.println("Do you want to continue? (y/n)");
+            if (!reader.nextLine().equals("y")) {
+                break;
+            }
+        }
     }
 }
